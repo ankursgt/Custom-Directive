fd4f7e5a5ab0ce6dfb22dd964a62483a62d4c0d7
Ankur <33163324+ankursgt@users.noreply.github.com>
2018-09-24 19:02:21 +0530

    Update generate.directive.ts

--- src/app/generate.directive.ts
+++ src/app/generate.directive.ts
@@ -7,32 +7,31 @@ export class GenerateDirective {
 
   constructor(private renderer: Renderer2) { }
 
-  @Input() set appGenerate(inv: number){
-    if(inv==1){
-      var newElement = document.createElement("INPUT");
-      newElement.setAttribute('id', "in");
-      this.renderer.appendChild(document.body,newElement)
-     
-      
-    }
-    else if(inv==2){
-      var newElement = document.createElement("SELECT");
-      newElement.setAttribute('id', "se");
-      var optionSelect=document.createElement("option");
-      optionSelect.text="Select";
-      newElement.appendChild(optionSelect)
-      this.renderer.appendChild(document.body,newElement)
-    }
-    else {
-      if(document.getElementById("in")){
-        document.body.removeChild(document.getElementById("in"));
-      }
-      else if(document.getElementById("se")){
-        document.body.removeChild(document.getElementById("se"));
-      }
-      else return;
-      
-      
+  @Input() set appGenerate(inv){
+    switch(parseInt(inv)){
+      case 1:
+        var newElement = document.createElement("INPUT");
+        newElement.setAttribute('id', "in");
+        this.renderer.appendChild(document.body,newElement)
+        break;
+        
+      case 2:
+        var newElement = document.createElement("SELECT");
+        newElement.setAttribute('id', "se");
+        var optionSelect=document.createElement("option");
+        optionSelect.text="Select";
+        newElement.appendChild(optionSelect)
+        this.renderer.appendChild(document.body,newElement);
+        break;
+        
+      default:
+        if(document.getElementById("in")){
+          document.body.removeChild(document.getElementById("in"));
+        }
+        else if(document.getElementById("se")){
+          document.body.removeChild(document.getElementById("se"));
+        }
+        else return;
     }
   }
 
